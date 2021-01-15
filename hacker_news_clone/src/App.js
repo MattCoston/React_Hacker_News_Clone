ca9efe769d4a700570547927eaa73c3984597772
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-01-14 17:45:44 -0700

    Feat: Added hello message.

--- hacker_news_clone/src/App.js
+++ hacker_news_clone/src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>FIRST COMMIT</h1>
+      <p>Hello world!</p>
     </div>
   );
 }
